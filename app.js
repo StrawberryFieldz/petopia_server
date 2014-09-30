1b7f503aec2af64e7a950af552fb091fa3478dec
Tyler <tyhlewis@gmail.com>
2014-09-29 17:40:43 -0700

    (feat) adds routing to app.js file

--- app.js
+++ app.js
@@ -3,13 +3,10 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-var test = require('./config/seed.js')
 
 var server = require('http').createServer(app);
 
-app.get('/stubData', function(request, response) {
-  response.send(test);
-});
+require('./routes')(app);
 
 server.listen(port, function() {
   console.log('Now listening on ', port);
