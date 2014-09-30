50cd08a3831d79da4c813837224087ea9ea87ab8
Dominic Czarnota <dczarno@gmail.com>
2014-09-29 22:22:19 -0700

    (feat) adds signup route

--- routes.js
+++ routes.js
@@ -4,5 +4,5 @@ module.exports = function(app) {
 
   app.use('/api/search', require('./api/search'));
   app.use('/api/users', require('./api/users'));
-
+  app.use('/signup', require('./auth/local'));
 };
