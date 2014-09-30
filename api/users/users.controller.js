a86eb1df1378aa16b15b2bfd69c6ba29ea913ae9
Dominic Czarnota <dczarno@gmail.com>
2014-09-29 20:45:26 -0700

    (feat) adds basic routing for users

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -1,7 +1,32 @@
 'use strict';
 
-var Users = require('../../config/seed.js');
+var Users = require('../../config/seed.js').users;
 
 exports.index = function(request, response) {
-  return response.json(200, Users);
+  var results = {
+    users: []
+  };
+
+  for(var key in Users){
+    var user = {};
+    user.username = Users[key].username;
+    results.users.push(user);
+  }
+
+  return response.json(200, results);
+};
+
+exports.byUsername = function(request, response){
+  var results = {
+    user: []
+  };
+
+  for(var key in Users){
+    if(Users[key].username === request.params.username){
+      results.user.push({ username: Users[key].username });
+      return response.json(200, results);
+    }
+  }
+
+  return response.json(200, 'User does not exist.');
 };
