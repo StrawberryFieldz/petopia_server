bdaae38c3a6cc98b852a8c92dbcc1414875301d5
Dominic Czarnota <dczarno@gmail.com>
2014-09-30 19:20:41 -0700

    (feat) adds body-parse to app.js and fixes routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8080;
+
+app.use(bodyParser());
 
 var server = require('http').createServer(app);
 
