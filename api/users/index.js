da24a01718d250881d3278862d837cd4fe77ea23
Dominic Czarnota <dczarno@gmail.com>
2014-09-30 19:19:58 -0700

    (feat) adds initial api/users files

--- api/users/index.js
+++ api/users/index.js
@@ -5,7 +5,10 @@ var controller = require('./users.controller.js');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:username', controller.byUsername);
+// router.get('/', controller.index);
+router.get('/:userid', controller.byUserId);
+router.get('/:email', controller.findUser);
+router.post('/', controller.createUser);
+
 
 module.exports = router;
