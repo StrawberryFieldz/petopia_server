642dfb80e1afc257b364b231a7be150c0b8a6610
Dominic Czarnota <dczarno@gmail.com>
2014-09-30 20:00:37 -0700

    (feat) starts refactor using bookshelf

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var app = express();
 var port = process.env.PORT || 8080;
 
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'petopia',
+    charset: 'utf8'
+  }
+});
+
+var bookshelf = require('bookshelf')(knex);
+
+app.set('bookshelf', bookshelf);
 app.use(bodyParser());
 
 var server = require('http').createServer(app);
@@ -14,3 +29,5 @@ require('./routes')(app);
 server.listen(port, function() {
   console.log('Now listening on ', port);
 });
+
+module.exports.bookshelf = bookshelf;
