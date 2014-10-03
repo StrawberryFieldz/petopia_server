5af6788feaed2b56c6e02fb0b1cb7989c4c03a73
Dominic Czarnota <dczarno@gmail.com>
2014-10-02 21:24:38 -0700

    (feat) adds local-signup as LocalStrategy

--- auth/local/passport.js
+++ auth/local/passport.js
@@ -1,22 +1,20 @@
 var LocalStrategy = require('passport-local').Strategy;
-var User = require('../../config/seed.js').users;
+var User = require('../../db/tables/db_users.js')
+var UserModel = require('../../db/tables/db_users.js').User;
 var passport = require('passport');
 
 module.exports = function(){
-  console.log("passport:", passport);
 
   // used to serialize the user for the session
   passport.serializeUser(function(user, done){
-    console.log('user: ', user.username);
+    console.log('user (from serialize): ', user.username);
     done(null, user);
   });
 
   // used to deserialize the user
   passport.deserializeUser(function(user, done){
-    // User.findById(id, function(err, user){
-      console.log(use)
-      done(null, user);
-    // });
+    console.log("user (from deserialize): ", user.username)
+    done(null, user);
   });
 
   passport.use('local-login', new LocalStrategy({
@@ -24,7 +22,6 @@ module.exports = function(){
     passwordField: 'password',
     passReqToCallback: true
   }, function(request, username, password, done){
-    console.log('passport.js cb');
       // Checks seed.js data
       for(var key in User){
         if(username === User[key]['username']){
@@ -37,24 +34,23 @@ module.exports = function(){
     return done(null, false);
   }));
 
+  passport.use('local-signup', new LocalStrategy(function(username, password, done){
+      UserModel.findOne({ username: username }, function(err, user){
+        if(err){
+          return done(err);
+        }
+        if(user){
+          return done(null, false, { message: 'Username already exists.' });
+        } else {
+          var newUser = {
+            username: username,
+            password: password
+          };
+
+          User.signupUser(newUser, done);
+        }
+      });
+    }
+  ));
 
 };
-
-// exports.setup = function(User){
-//   passport.use(new LocalStrategy(
-//     function(username, password, done){
-//       User.findOne({ username: username }, function(err, use){
-//         if(err){
-//           return done(err);
-//         }
-//         if(!user){
-//           return done(null, false, { message: 'Incorrect username.' });
-//         }
-//         if(!user.validPassword(password)){
-//           return done(null, false, { message: 'Incorrect password.' });
-//         }
-//         return done(null, user);
-//       });
-//     }
-//   ));
-// };
