56568358eb629d32c494ad41da9632958542703f
Dominic Czarnota <dczarno@gmail.com>
2014-10-02 19:10:06 -0700

    (feat) adds users files in api

--- api/users/index.js
+++ api/users/index.js
@@ -2,13 +2,15 @@
 
 var express = require('express');
 var controller = require('./users.controller.js');
+var appjs = require('../../app.js').passport;
+var passport = require('../../auth/local/passport.js')(appjs);
 
 var router = express.Router();
 
 // router.get('/', controller.index);
-router.get('/:userid', controller.byUserId);
-router.get('/:email', controller.findUser);
-router.post('/', controller.createUser);
-
+// router.get('/:userid', controller.byUserId);
+// router.get('/:email', controller.findUser);
+router.post('/signup', controller.createUser);
+// router.post('/login', passport.authenticate('local-login'));
 
 module.exports = router;
