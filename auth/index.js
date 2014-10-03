e699d049ea3a9477f77e70a5609cd2c53ae1ccde
Tyler <tyhlewis@gmail.com>
2014-10-03 14:58:33 -0700

    (cleanup) removes unused files and unnecessary code

--- auth/index.js
+++ auth/index.js
@@ -1,12 +1,1 @@
-// var passport = require('passport');
-// var express = require('express');
 
-// // require('./local/passport').setup();
-// // require('./facebook/passport').setup();
-
-// var router = express.Router();
-
-// router.use('/local', require('./local'));
-// router.use('/facebook', require('./facebook'));
-
-// module.exports = router;
