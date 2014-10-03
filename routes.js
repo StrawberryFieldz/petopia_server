da359fd2d0ff524d8b798313554b31a5a88ccdb8
Dominic Czarnota <dczarno@gmail.com>
2014-10-02 19:10:54 -0700

    (feat) set up basic oauth using seed.js data

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-module.exports = function(app) {
-
+module.exports = function(app, passport) {
   app.use('/api/search', require('./api/search'));
   app.use('/api/users', require('./api/users'));
+  app.post('/login', passport.authenticate('local-login'));
 };
