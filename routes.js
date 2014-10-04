63168d8e7ca2e706b0087846575837183ab8471e
Dominic Czarnota <dczarno@gmail.com>
2014-10-03 17:05:38 -0700

    (test) tests MongoLab get request

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@
 module.exports = function(app, passport) {
 
   app.use('/api/search', require('./api/search'));
-
+  app.get('/login', require('./db/tables/db_users.js').testUsers);
   app.post('/signup', passport.authenticate('local-signup'));
   app.post('/login', passport.authenticate('local-login'));
   
