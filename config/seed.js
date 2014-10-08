aadc7ebdc64d4bfe8bf8b3a7a8011be9080d8020
Wil A <andradewilker@gmail.com>
2014-10-07 21:41:04 -0700

    Added additional info to server stub data.

--- config/seed.js
+++ config/seed.js
@@ -7,7 +7,10 @@ module.exports.search = {
     zip: 94103,
     photo: "https://avatars1.githubusercontent.com/u/7740415?v=2&s=400",
     cost: 35,
-    rating: 4
+    rating: 4,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   },
   1: {
     name: "Wil",
@@ -15,7 +18,10 @@ module.exports.search = {
     zip: 94103,
     photo: "https://avatars2.githubusercontent.com/u/7147319?v=2&s=400",
     cost: 99,
-    rating: 5
+    rating: 5,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   },
   2: {
     name: "Dominic",
@@ -23,7 +29,10 @@ module.exports.search = {
     zip: 84732,
     photo: "https://avatars2.githubusercontent.com/u/6237873?v=2&s=400",
     cost: 60,
-    rating: 4
+    rating: 4,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   },
   3: {
     name: "Jeff",
@@ -31,7 +40,10 @@ module.exports.search = {
     zip: 03062,
     photo: "https://avatars2.githubusercontent.com/u/7004952?v=2&s=400",
     cost: 40,
-    rating: 1
+    rating: 1,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   },
   4: {
     name: "Bob",
@@ -39,7 +51,10 @@ module.exports.search = {
     zip: 12345,
     photo: "http://www.mateoj.com/bayside/wp-content/uploads/2014/07/521013543_1385596410.jpg",
     cost: 15,
-    rating: 2
+    rating: 2,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   },
   5: {
     name: "Sam",
@@ -47,7 +62,10 @@ module.exports.search = {
     zip: 94103,
     photo: "http://static.ddmcdn.com/gif/dog-breed-dog-breed-pictures1.jpg",
     cost: 65,
-    rating: 3
+    rating: 3,
+    bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
+            dogs:true,
+            cats: true
   }
 };
 
