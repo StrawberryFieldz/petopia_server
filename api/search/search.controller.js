b9d2fb19cb1039ba593be665300c9c0778e5fdfd
Wil A <andradewilker@gmail.com>
2014-10-10 11:24:48 -0700

    users api will now include name property in Mongo query results.

--- api/search/search.controller.js
+++ api/search/search.controller.js
@@ -4,14 +4,24 @@ var UserModel = require('../../db/tables/db_users.js').User;
 'use strict';
 
 exports.index = function(request, response) {
-  return response.json(200, "Nothing to see here");
+  UserModel.find({}, 
+    'username location', 
+    function(err, data){
+      if(err){
+        console.log("no sitters found in database", err)
+        response.send(404);
+      }
+      if(data){
+        return response.json(200, data);
+      }
+
+    });
 };
 
 exports.byLocation = function(request, response) {
-  var results = [];
   var location = request.params.location;
-  UserModel.find({ location: location }, 
-    'username location zip photo cost rating bio dogs cats rating', 
+  UserModel.find({ location: location, isSitter:true }, 
+    'username name location zip photo cost rating bio dogs cats rating', 
     function(err, data){
       if(err){
         console.log("no sitters found in database", err)
